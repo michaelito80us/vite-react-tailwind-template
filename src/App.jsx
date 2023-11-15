878cc0eaa192decb77606756a85858cc6094f4e5
Michael Epelboim <mepelboim@outlook.com>
2023-11-15 14:59:31 +0100

    fixed tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flex flex-col  items-center justify-center p-8 text-center min-h-screen">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="h-24 p-6 hover:drop-shadow-lg"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="h-24 p-6 hover:drop-shadow-2xl"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="text-5xl">Vite + React</h1>
+      <div className="p-8">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,10 +32,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-gray-500">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
